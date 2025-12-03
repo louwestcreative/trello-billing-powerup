a5789c87bccad6d09cb1c2c2125f26fc9cb1eed3
louwestcreative <info@louwestcreative.com>
2025-12-02 18:58:19 -0800

    Update client.js

--- client.js
+++ client.js
@@ -1,31 +1,14 @@
-var Promise = TrelloPowerUp.Promise;
-
 TrelloPowerUp.initialize({
-  'card-buttons': function(t, options) {
-    return [
-      {
-        icon: "https://louwestcreative.github.io/trello-billing-powerup/icon.png",
-        text: "Add Payment",
-
-        callback: function(t) {
-          return t.modal({
-            title: "Record Payment",
-            url: "https://louwestcreative.github.io/trello-billing-powerup/payment-entry.html",
-            height: 420
-          });
-        }
+  'card-buttons': function(t, opts) {
+    return [{
+      text: 'Billing',
+      callback: function(t) {
+        return t.modal({
+          url: 'https://louwestcreative.github.io/trello-billing-powerup/client.html',
+          height: 420,
+          title: 'Billing'
+        });
       }
-    ];
-  },
-
-  'card-badges': function(t, options) {
-    return t.get('card', 'shared', 'payment')
-      .then(function(payment) {
-        if (!payment) return [];
-        return [{
-          text: `$${payment.amount}`,
-          color: "blue"
-        }];
-      });
+    }];
   }
 });
