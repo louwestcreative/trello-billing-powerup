8f7cd4642938f54fb0affb43e9f56972d853310a
louwestcreative <info@louwestcreative.com>
2025-12-02 19:26:57 -0800

    Update client.js

--- client.js
+++ client.js
@@ -1,14 +1,10 @@
 TrelloPowerUp.initialize({
-  'card-buttons': function(t, opts) {
-    return [{
-      text: 'Billing',
-      callback: function(t) {
-        return t.modal({
-          url: 'https://louwestcreative.github.io/trello-billing-powerup/client.html',
-          height: 420,
-          title: 'Billing'
-        });
-      }
-    }];
-  }
+  'card-buttons': (t, opts) => [{
+    text: 'Billing',
+    callback: t => t.modal({
+      url: 'https://louwestcreative.github.io/trello-billing-powerup/client.html',
+      height: 420,
+      title: 'Billing'
+    })
+  }]
 });
