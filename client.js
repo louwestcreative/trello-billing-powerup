fe951ccc66c1610b7d089405dc0317b05451073e
louwestcreative <info@louwestcreative.com>
2025-12-22 15:07:47 -0800

    Update client.js

--- client.js
+++ client.js
@@ -1,45 +1,15 @@
-// client.js
-TrelloPowerUp.initialize({
+var t = TrelloPowerUp.iframe();
+
+t.initialize({
   'card-back-section': function(t, opts) {
     return {
-      title: 'Billing Information',
-      icon: 'https://cdn-icons-png.flaticon.com/512/263/263115.png',
+      title: 'Billing Info',
+      icon: 'https://louwestcreative.github.io/trello-billing-powerup/coin.png',
       content: {
         type: 'iframe',
         url: t.signUrl('./index.html'),
-        height: 300
+        height: 200
       }
     };
-  },
-
-  'card-buttons': function(t, options) {
-    return [
-      {
-        icon: 'https://cdn-icons-png.flaticon.com/512/1828/1828919.png',
-        text: 'Add Charge',
-        callback: function(t) {
-          return t.modal({
-            url: t.signUrl('./modal.html?type=charge'),
-            title: 'Add Charge',
-            height: 320,
-            fullscreen: false
-          });
-        }
-      },
-      {
-        icon: 'https://cdn-icons-png.flaticon.com/512/1828/1828919.png',
-        text: 'Add Payment',
-        callback: function(t) {
-          return t.modal({
-            url: t.signUrl('./modal.html?type=payment'),
-            title: 'Add Payment',
-            height: 320,
-            fullscreen: false
-          });
-        }
-      }
-    ];
-  },
-
-  'card-badges': async function(t, options) {
-    const data = awa
+  }
+});
